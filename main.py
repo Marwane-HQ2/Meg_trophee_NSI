6b0de149d88731e2c11dec07ef6218782209c54c
Marwane <marwaneseffah@gmail.com>
2025-03-02 14:54:32 +0100

    Gestion des déplacements du joueur

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ NOIR = (0, 0, 0)
 BLANC = (255, 255, 255)
 
 # TEXTE 
-police_titre = pygame.font.Font("DeliciousHandrawn-Regular.ttf", 45) # CREER UNE POLICE 
-police_principale = pygame.font.Font("DeliciousHandrawn-Regular.ttf", 35)
+police_titre = pygame.font.Font("IndieFlower-Regular.ttf", 45) # CREER UNE POLICE 
+police_principale = pygame.font.Font("IndieFlower-Regular.ttf", 35)
 
 # INFOS ECRAN
 SCREEN_WIDTH = 500
@@ -28,16 +28,18 @@ pygame.display.set_caption("Meg")
 
 
 # ----------------------------- CLASSES -----------------------------
-class Boite(pygame.sprite.Sprite):
+class Joueur(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
 
         # CHARGEMENT DE L'IMAGE
-        self.image = pygame.image.load("box.png")
+        self.image = pygame.image.load("assets/player.png")
         self.rect = self.image.get_rect()
 
         # DEPLACEMENTS ET POSITIONS
-        self.rect.center = (SCREEN_WIDTH//2 , SCREEN_HEIGHT//2)
+        self.rect.center = (SCREEN_WIDTH//2 , SCREEN_HEIGHT//2) # POSTITION DE DEPART
+        self.delta_x = 4
+        self.delta_y = 4
 
     def afficher(self, surface):
         """
@@ -53,18 +55,28 @@ class Boite(pygame.sprite.Sprite):
         """
         pressed_keys = pygame.key.get_pressed()
         if pressed_keys[K_LEFT]:
-            if self.rect.center == self.droite: # SI LA BOITE EST A GAUCHE
-                self.rect.center = self.gauche
+            if self.rect.left > 0: # SI LA BOITE EST A GAUCHE
+                self.rect.move_ip(-1*self.delta_x, 0)
 
         if pressed_keys[K_RIGHT]:
-            if self.rect.center == self.gauche:
-                self.rect.center = self.droite
-                    
+            if self.rect.right < SCREEN_WIDTH:
+                self.rect.move_ip(self.delta_x, 0)
+
+        if pressed_keys[K_UP]:
+            if self.rect.top > 0:
+                self.rect.move_ip(0, -1*self.delta_y)
+        
+        if pressed_keys[K_DOWN]:
+            if self.rect.bottom < SCREEN_HEIGHT :
+                self.rect.move_ip(0, self.delta_y)
 
 
 # ----------------------------- VARIABLES -----------------------------
 # ------- LUTINS ET ASSETS -------
+JOUEUR = Joueur() # LE JOUEUR
+
 all_sprites = pygame.sprite.Group() # GROUPE UTILISE POUR AFFICHER
+all_sprites.add(JOUEUR)
 
 # ------- TEMPS -------
 horloge_globale = pygame.time.get_ticks()
@@ -74,7 +86,6 @@ horloge_globale = pygame.time.get_ticks()
 # ------- COLLISIONS -------
 
 # ------- TEXTE -------
-
 def bulle_info(texte):
     pass
 
@@ -85,14 +96,12 @@ while True:
     # OBTENIR L'HEURE QU'IL EST A CHAQUE ITERATION
     heure = pygame.time.get_ticks()
 
-    # ----------------- QUITTER JEU -----------------
-    
-    # SI APPUI SUR LA CROIX
+    # ----------------- GESTION DES EVENNEMENTS-----------------    
     for event in pygame.event.get():              
-        if event.type == QUIT:
+        if event.type == QUIT: # SI APPUI SUR LA CROIX
             pygame.quit()
             sys.exit()
-        if event.type == MOUSEBUTTONDOWN:
+        if event.type == MOUSEBUTTONDOWN: # AFFICHER LA POSITION DU CURSEUR QUAND ON CLIQUE. 
             print(pygame.mouse.get_pos())
 
     # QUITTE LE JEU SI APPUI SUR ESC
@@ -101,10 +110,10 @@ while True:
         sys.exit()
 
     # ----------------- JEU -----------------
-
+    # PERMETTRE LE DEPLACEMENT DU JOUEUR
+    JOUEUR.deplacer()
 
     # ----------------- AFFICHAGE -----------------
-
     # REPEINDRE L'ECRAN
     DISPLAYSURFACE.fill(BLANC)
 
@@ -115,5 +124,3 @@ while True:
     # REAFFICHER L'ECRAN
     pygame.display.update()
     FramePerSec.tick(FPS)
-
-play()
